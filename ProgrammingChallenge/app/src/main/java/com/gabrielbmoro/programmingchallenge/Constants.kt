86090927c2b1171afa23b6831f75501030cd1c31
gbmoro <gbmoro@inf.ufrgs.br>
2018-08-29 23:41:34 -0300

    Fragments, RxJava and MVP pattern

--- ProgrammingChallenge/app/src/main/java/com/gabrielbmoro/programmingchallenge/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.gabrielbmoro.programmingchallenge
-
-object Constants {
-
-    val BASE_URL : String = "https://api.themoviedb.org/3/"
-    val API_KEY : String = "755e0c67ac2fa886e775fb9057f0a32f"
-}
--- ProgrammingChallenge/app/src/main/java/com/gabrielbmoro/programmingchallenge/ui/MainActivity.kt
+++ ProgrammingChallenge/app/src/main/java/com/gabrielbmoro/programmingchallenge/ui/MainActivity.kt
@@ -1,50 +1,41 @@
 package com.gabrielbmoro.programmingchallenge.ui
 
-import android.annotation.SuppressLint
-import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.gabrielbmoro.programmingchallenge.Constants
 import com.gabrielbmoro.programmingchallenge.R
-import com.gabrielbmoro.programmingchallenge.api.ApiServiceInterface
-import com.gabrielbmoro.programmingchallenge.models.Page
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import okhttp3.OkHttpClient
-
+import com.gabrielbmoro.programmingchallenge.api.ApiServiceAccess
+import com.gabrielbmoro.programmingchallenge.models.Movie
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 
 
 class MainActivity : AppCompatActivity() {
+
+    var mlstMoviesFiltered : ArrayList<Movie>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val api = ApiServiceAccess()
+        api.getMovies("vote_average.desc")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        {
+                            if(it.mlstResults != null) mlstMoviesFiltered = ArrayList(it.mlstResults!!)
+                        },
+                        {
+                            it.printStackTrace()
+                        },{
+                            //ui
+                            mlstMoviesFiltered?.forEach { Log.d("teste", it.toString()) }
+                        }
+                )
         //?sort_by=vote_average.desc
         //?sort_by=popularity.desc
 
-        val asyncTask = @SuppressLint("StaticFieldLeak")
-        object : AsyncTask<Void, Void, Void>() {
-            override fun doInBackground(vararg p0: Void?): Void? {
-                val call = ApiServiceInterface.create().getMovies(Constants.API_KEY, "vote_average.desc")
-                call.enqueue(object : Callback<Page> {
-                    override fun onFailure(call: Call<Page>, t: Throwable) {
-                        Log.d("REsponse", t.toString())
-                    }
-
-                    override fun onResponse(call: Call<Page>, response: Response<Page>) {
-                        val pgPageReceived = response.body()
-                        if(pgPageReceived != null) {
-                            Log.d("REsponse", "Sucesso olha ai: ${pgPageReceived.mnTotalResults}")
-                        }
-                    }
-                })
-                return null
-            }
-        }
-        asyncTask.execute()
 
     }
 }
