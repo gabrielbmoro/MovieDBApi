2af3a49c847a793b08f422f1cd12421beeab7e77
gbmoro <gbmoro@inf.ufrgs.br>
2018-09-01 15:21:16 -0300

    Connection status monitor

--- ProgrammingChallenge/app/src/main/java/com/gabrielbmoro/programmingchallenge/ui/homescreen/HomeScreen.kt
+++ ProgrammingChallenge/app/src/main/java/com/gabrielbmoro/programmingchallenge/ui/homescreen/HomeScreen.kt
@@ -1,15 +1,19 @@
 package com.gabrielbmoro.programmingchallenge.ui.homescreen
 
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.os.Bundle
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentPagerAdapter
 import com.gabrielbmoro.programmingchallenge.R
+import com.gabrielbmoro.programmingchallenge.network_monitor.ConnectivityReceiver
 import com.gabrielbmoro.programmingchallenge.ui.homescreen.pages.FavoriteMoviesFragment
 import com.gabrielbmoro.programmingchallenge.ui.homescreen.pages.PopularMoviesFragment
 import com.gabrielbmoro.programmingchallenge.ui.homescreen.pages.TopRatedFragment
 import me.relex.circleindicator.CircleIndicator
+import android.support.design.widget.Snackbar
 
 /**
  * This contract provides two interfaces:
@@ -21,18 +25,14 @@ import me.relex.circleindicator.CircleIndicator
  */
 interface HomeScreenContract {
     /**
-     * Presenter defines the load movies action.
-     * @author Gabriel Moro
-     * @since 2018-08-30
-     */
-    interface Presenter { }
-    /**
      * View defines the setup and update screen operations.
      * @author Gabriel Moro
      * @since 2018-08-30
      */
     interface View{
         fun loadPages()
+        fun showSnackBar(astrMessage : String)
+        fun hideSnackBar()
     }
 }
 
@@ -42,11 +42,11 @@ interface HomeScreenContract {
  * @author Gabriel Moro
  * @since 2018-08-30
  */
-class HomeScreenActivity : AppCompatActivity(), HomeScreenContract.View {
+class HomeScreenActivity : AppCompatActivity(), HomeScreenContract.View, ConnectivityReceiver.ConnectivityReceiverListener  {
 
-    private var presenter    : HomeScreenContract.Presenter? = null
     private var mvwViewPager : ViewPager?                    = null
     private var mciIndicator : CircleIndicator?              = null
+    private var msbSnackBar  : Snackbar?                     = null
 
     /**
      * This method is called before screen creation.
@@ -62,6 +62,22 @@ class HomeScreenActivity : AppCompatActivity(), HomeScreenContract.View {
         mciIndicator = findViewById(R.id.indicator)
 
         loadPages()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        registerReceiver(ConnectivityReceiver(), IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+        ConnectivityReceiver.connectivityReceiverListener = this
+    }
+
+    /**
+     * Callback will be called when there is change
+     */
+    override fun onNetworkConnectionChanged(abisConnected: Boolean) {
+        if(!abisConnected)
+            showSnackBar(resources.getString(R.string.offlinenow))
+        else
+            hideSnackBar()
     }
 
     /**
@@ -87,9 +103,25 @@ class HomeScreenActivity : AppCompatActivity(), HomeScreenContract.View {
         mciIndicator?.setViewPager(mvwViewPager)
         mvwViewPager?.adapter?.registerDataSetObserver(mciIndicator?.dataSetObserver)
     }
-}
 
-class HomeScreenPresenter(aview : HomeScreenContract.View) : HomeScreenContract.Presenter {
-    private val view : HomeScreenContract.View = aview
-    // object
+    /**
+     * This method shows the snackbar with message
+     * @author Gabriel Moro
+     * @since 2018-08-30
+     */
+    override fun showSnackBar(astrMessage: String) {
+        msbSnackBar = Snackbar.make(findViewById(R.id.clParentView), astrMessage, Snackbar.LENGTH_LONG) //Assume "rootLayout" as the root layout of every activity.
+        msbSnackBar?.duration = Snackbar.LENGTH_INDEFINITE
+        msbSnackBar?.show()
+    }
+
+
+    /**
+     * Hide the snackbar component.
+     * @author Gabriel Moro
+     * @since 2018-08-30
+     */
+    override fun hideSnackBar() {
+        msbSnackBar?.dismiss()
+    }
 }
