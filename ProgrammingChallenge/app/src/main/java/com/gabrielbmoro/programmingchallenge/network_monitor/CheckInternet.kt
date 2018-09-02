79bdfab8adfe53873226f94290bd3087e7677a38
gbmoro <gbmoro@inf.ufrgs.br>
2018-09-02 11:23:46 -0300

    Keeping onResume active when activity changes its orientation

---
+++ ProgrammingChallenge/app/src/main/java/com/gabrielbmoro/programmingchallenge/network_monitor/CheckInternet.kt
@@ -0,0 +1,28 @@
+package com.gabrielbmoro.programmingchallenge.network_monitor
+
+import android.content.Context
+import android.net.ConnectivityManager
+
+/**
+ * This class is used to check the internet state.
+ * @author Gabriel Moro
+ * @since 2018-08-30
+ */
+object CheckInternet {
+
+    /**
+     * Check the internet state.
+     * @author Gabriel Moro
+     * @since 2018-08-30
+     */
+    fun checkInternet(actxContext : Context) : Boolean {
+        val cm = actxContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = cm.activeNetworkInfo
+
+        return if (activeNetwork != null) {
+            (activeNetwork.type == ConnectivityManager.TYPE_WIFI || activeNetwork.type == ConnectivityManager.TYPE_MOBILE)
+        } else {
+            false
+        }
+    }
+}
