2fe123cff56d9f024df11331497a7df946ac4c98
Gabriel Moro <gabrielbronzattimoro.es@gmail.com>
2020-04-18 10:55:01 -0300

    Organizing better the dependency and removing unused dependencies

--- ProgrammingChallenge/app/build.gradle.kts
+++ ProgrammingChallenge/app/build.gradle.kts
@@ -33,37 +33,27 @@ android {
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.3.71")
-    //noinspection GradleCompatible
+
     implementation("androidx.appcompat:appcompat:1.1.0")
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
-    implementation("com.google.android.material:material:1.1.0")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0-rc01")
-    //retrofit + gson
-    implementation("com.squareup.retrofit2:retrofit:2.6.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.6.0")
-    // RXJava
-    implementation("io.reactivex.rxjava2:rxjava:2.2.9")
-    implementation("io.reactivex.rxjava2:rxandroid:2.1.1")
-    //circle indicator
-    implementation("me.relex:circleindicator:1.2.2@aar")
     implementation("androidx.recyclerview:recyclerview:1.1.0")
-    //autofittextview
-    implementation("me.grantland:autofittextview:0.2.1")
-    //Glide
-    implementation("com.github.bumptech.glide:glide:4.11.0")
-    //cardview
     implementation("androidx.cardview:cardview:1.0.0")
-    //room
     implementation("androidx.room:room-runtime:2.2.5")
     kapt("androidx.room:room-compiler:2.2.5")
     implementation("androidx.room:room-ktx:2.2.5")
-
-    // Lifecycle viewmodel
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
 
-    // Koin
+    implementation("com.google.android.material:material:1.1.0")
+
+    implementation("com.squareup.retrofit2:retrofit:2.6.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.6.0")
+
+    implementation("com.github.bumptech.glide:glide:4.11.0")
+
     implementation("org.koin:koin-androidx-scope:2.0.1")
     implementation("org.koin:koin-androidx-viewmodel:2.0.1")
     implementation("org.koin:koin-androidx-ext:2.0.1")
