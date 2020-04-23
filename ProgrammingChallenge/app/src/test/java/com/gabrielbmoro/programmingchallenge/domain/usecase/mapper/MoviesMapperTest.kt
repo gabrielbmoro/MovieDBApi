ca402787f2899ab06c18d583d6d3d5e73226b5e0
Gabriel Moro <gabrielbronzattimoro.es@gmail.com>
2020-04-22 21:40:00 -0300

    Adding some unit tests to the mapper

--- ProgrammingChallenge/app/src/test/java/com/gabrielbmoro/programmingchallenge/domain/usecase/mapper/MoviesMapperTest.kt
+++ ProgrammingChallenge/app/src/test/java/com/gabrielbmoro/programmingchallenge/domain/usecase/mapper/MoviesMapperTest.kt
@@ -1,12 +1,32 @@
 package com.gabrielbmoro.programmingchallenge.domain.usecase.mapper
 
+import com.gabrielbmoro.programmingchallenge.domain.model.Page
+import com.gabrielbmoro.programmingchallenge.repository.api.response.PageResponse
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 
 class MoviesMapperTest {
 
     @Test
-    fun mapper_nullResponse_ReturnsNull() {
-        assertThat(MoviesMapper.map(null)).isNull()
+    fun whenTheResponseIsNull() {
+        val given = null
+        val result = MoviesMapper.map(given)
+        assertThat(result).isNull()
+    }
+
+    @Test
+    fun whenTheResponseIsEmpty() {
+        val given = PageResponse(
+                totalResults = 0,
+                totalPages = 0,
+                page = 0,
+                results = emptyList()
+        )
+        val result = MoviesMapper.map(given)
+        val expectedResult = Page(
+                movies = emptyList(),
+                hasMorePages = false
+        )
+        assertThat(result).isEqualTo(expectedResult)
     }
 }
