bfc1c6d692e11ab50d8a0c7bf67b2c888b627b80
moro <moro@devgrid.co.uk>
2020-08-16 12:51:06 -0300

    Removing the findViewById in ViewHolder to use the synthetic instead of

--- ProgrammingChallenge/app/src/main/java/com/gabrielbmoro/programmingchallenge/presentation/components/adapter/MovieViewHolder.kt
+++ ProgrammingChallenge/app/src/main/java/com/gabrielbmoro/programmingchallenge/presentation/components/adapter/MovieViewHolder.kt
@@ -2,31 +2,23 @@ package com.gabrielbmoro.programmingchallenge.presentation.components.adapter
 
 import android.app.Activity
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
 import com.gabrielbmoro.programmingchallenge.presentation.util.setImagePath
 import androidx.recyclerview.widget.RecyclerView
-import com.gabrielbmoro.programmingchallenge.R
 import com.gabrielbmoro.programmingchallenge.domain.model.Movie
-import com.gabrielbmoro.programmingchallenge.presentation.components.FiveStarsComponent
 import com.gabrielbmoro.programmingchallenge.presentation.detailedScreen.MovieDetailedActivity
+import kotlinx.android.synthetic.main.view_holder_movie_card.view.*
 
 class MovieViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
 
-    private val ivPoster: ImageView? = view.findViewById(R.id.ivPoster)
-    private val tvTitle: TextView? = view.findViewById(R.id.tvTitle)
-    private val tvReleaseDate: TextView? = view.findViewById(R.id.tvReleaseDate)
-    private val fiveStarsComponent: FiveStarsComponent? = view.findViewById(R.id.fiveStarsComponent)
-
     fun bind(movie: Movie) {
-        ivPoster?.setImagePath(movie.posterPath)
-        tvTitle?.text = movie.title
-        tvReleaseDate?.text = movie.releaseDate
-        fiveStarsComponent?.setVotesAvg(movie.votesAverage)
+        view.view_holder_movie_card_poster.setImagePath(movie.posterPath)
+        view.view_holder_movie_card_title.text = movie.title
+        view.view_holder_movie_card_release_date.text = movie.releaseDate
+        view.view_holder_movie_card_five_stars_component.setVotesAvg(movie.votesAverage)
         view.setOnClickListener {
             (view.context as? Activity)?.let { activity ->
-                ivPoster?.let {
-                    MovieDetailedActivity.startActivity(activity, movie, ivPoster)
+                view.view_holder_movie_card_poster?.let {
+                    MovieDetailedActivity.startActivity(activity, movie, view.view_holder_movie_card_poster)
                 }
             }
         }
