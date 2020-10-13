6c7882aa98a7158e34df8b3c40919405bc69558d
DESKTOP-C0EL5GV\gabri <gabrielbronzattimoro@gmail.com>
2020-10-12 22:47:44 -0300

    Creating converters

--- flutter-version/ProgrammingChallenge/movie_db_app/lib/domain/model/movie.dart
+++ flutter-version/ProgrammingChallenge/movie_db_app/lib/domain/model/movie.dart
@@ -17,4 +17,35 @@ class Movie {
     @required String releaseDate,
     bool isFavorite = false,
   });
+
+  factory Movie.fromJson(Map<String, dynamic> json) {
+    final int votes = json['vote_count'];
+    final bool isVideo = json['video'];
+    final double votesAverage = json['vote_average'];
+    final String title = json['title'];
+    final double popularity = json['popularity'];
+    final String posterPath = json['poster_path'];
+    final String originalLanguage = json['original_language'];
+    final String originalTitle = json['original_title'];
+    final String backdropPath = json['backdrop_path'];
+    final bool isAdult = json['adult'];
+    final String overview = json['overview'];
+    final String releaseDate = json['release_date'];
+
+    return Movie(
+      votes: votes,
+      isVideo: isVideo,
+      votesAverage: votesAverage,
+      title: title,
+      popularity: popularity,
+      postPath: posterPath,
+      originalLanguage: originalLanguage,
+      originalTitle: originalTitle,
+      backdropPath: backdropPath,
+      isAdult: isAdult,
+      overview: overview,
+      releaseDate: releaseDate,
+      id: null,
+    );
+  }
 }
--- flutter-version/ProgrammingChallenge/movie_db_app/lib/domain/model/page.dart
+++ flutter-version/ProgrammingChallenge/movie_db_app/lib/domain/model/page.dart
@@ -7,4 +7,15 @@ class Page {
     @required List<Movie> movies,
     @required bool hasMorePages,
   });
+
+  factory Page.fromJson(Map<String, dynamic> json) {
+    int page = json['page'];
+    int totalPages = json['total_pages'];
+    List<Map<String, dynamic>> results = json['results'];
+    List<Movie> movies = [];
+
+    movies.addAll(results.map((e) => Movie.fromJson(e)));
+
+    return Page(movies: movies, hasMorePages: (page < totalPages));
+  }
 }
