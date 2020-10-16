bcd6e8bca18341f25ce3acf660aa1746c6db4054
DESKTOP-C0EL5GV\gabri <gabrielbronzattimoro@gmail.com>
2020-10-16 20:45:48 -0300

    Creating the class properties that were missing

--- flutter-version/ProgrammingChallenge/movie_db_app/lib/domain/model/movie.dart
+++ flutter-version/ProgrammingChallenge/movie_db_app/lib/domain/model/movie.dart
@@ -1,29 +1,55 @@
 import 'package:flutter/material.dart';
 
 class Movie {
-  Movie({
-    @required int id,
-    @required double votesAverage,
-    @required String title,
-    @required bool isVideo,
-    @required int votes,
-    @required double popularity,
-    @required String postPath,
-    @required String originalLanguage,
-    @required String originalTitle,
-    @required String backdropPath,
-    @required bool isAdult,
-    @required String overview,
-    @required String releaseDate,
-    bool isFavorite = false,
-  });
+  int id;
+  int votesAverage;
+  String title;
+  bool isVideo;
+  int votes;
+  int popularity;
+  String postPath;
+  String originalLanguage;
+  String originalTitle;
+  String backdropPath;
+  bool isAdult;
+  String overview;
+  String releaseDate;
+  bool isFavorite = false;
+
+  Movie(
+      {@required int votesAverage,
+      @required String title,
+      @required bool isVideo,
+      @required int votes,
+      @required int popularity,
+      @required String postPath,
+      @required String originalLanguage,
+      @required String originalTitle,
+      @required String backdropPath,
+      @required bool isAdult,
+      @required String overview,
+      @required String releaseDate}) {
+    this.id = id;
+    this.votesAverage = votesAverage;
+    this.title = title;
+    this.isVideo = isVideo;
+    this.votes = votes;
+    this.popularity = popularity;
+    this.postPath = postPath;
+    this.originalLanguage = originalLanguage;
+    this.originalTitle = originalTitle;
+    this.backdropPath = backdropPath;
+    this.isAdult = isAdult;
+    this.overview = overview;
+    this.releaseDate = releaseDate;
+  }
 
   factory Movie.fromJson(Map<String, dynamic> json) {
     final int votes = json['vote_count'];
     final bool isVideo = json['video'];
-    final double votesAverage = json['vote_average'];
+    final int votesAverage = 0; //json['vote_average'];
     final String title = json['title'];
-    final double popularity = json['popularity'];
+    final int popularity = 0; //json['popularity'];
     final String posterPath = json['poster_path'];
     final String originalLanguage = json['original_language'];
     final String originalTitle = json['original_title'];
@@ -45,7 +71,11 @@ class Movie {
       isAdult: isAdult,
       overview: overview,
       releaseDate: releaseDate,
-      id: null,
     );
+  }
+
+  @override
+  String toString() {
+    return "$title -> popularity $popularity, votes $votesAverage";
   }
 }
--- flutter-version/ProgrammingChallenge/movie_db_app/lib/domain/model/page.dart
+++ flutter-version/ProgrammingChallenge/movie_db_app/lib/domain/model/page.dart
@@ -3,19 +3,38 @@ import 'package:flutter/material.dart';
 import 'movie.dart';
 
 class Page {
+  List<Movie> _movieList;
+  bool _hasMorePages;
+
   Page({
     @required List<Movie> movies,
     @required bool hasMorePages,
-  });
+  }) {
+    _movieList = movies;
+    _hasMorePages = hasMorePages;
+  }
 
   factory Page.fromJson(Map<String, dynamic> json) {
     int page = json['page'];
     int totalPages = json['total_pages'];
-    List<Map<String, dynamic>> results = json['results'];
+    List<dynamic> results = json['results'];
     List<Movie> movies = [];
 
-    movies.addAll(results.map((e) => Movie.fromJson(e)));
+    results.forEach((element) {
+      var movie = Movie.fromJson(element);
+      if (movie != null) {
+        movies.add(movie);
+      }
+    });
 
     return Page(movies: movies, hasMorePages: (page < totalPages));
+  }
+
+  moviesList() {
+    return _movieList;
+  }
+
+  hasMorePages() {
+    return _hasMorePages;
   }
 }
