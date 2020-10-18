6d638586a96956104bf5d52e05194329676578f7
Gabriel Bronzatti Moro <gabrielbronzattimoro@gmail.com>
2020-10-18 15:40:18 -0300

    Using PageView to manage the pagination

--- flutter-version/ProgrammingChallenge/movie_db_app/lib/presentation/components/movieList/movie_list.dart
+++ flutter-version/ProgrammingChallenge/movie_db_app/lib/presentation/components/movieList/movie_list.dart
@@ -1,21 +1,71 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:movie_db_app/core/use_case_factory.dart';
+import 'package:movie_db_app/domain/model/page.dart' as page;
 import 'package:movie_db_app/domain/model/movie.dart';
-
+import 'package:movie_db_app/domain/model/movie_type.dart';
 import 'movie_list_item.dart';
 
 // ignore: must_be_immutable
-class MovieList extends StatelessWidget {
-  List<Movie> _movies;
+class MovieList extends StatefulWidget {
+  _MovieListState _state;
 
-  MovieList(List<Movie> movies) {
-    _movies = movies;
+  MovieList(MovieType movieType) {
+    _state = _MovieListState(movieType);
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    return _state;
+  }
+}
+
+class _MovieListState extends State<MovieList> {
+  List<Movie> _movies = [];
+  MovieType _type;
+  int _currentPageSelected = 1;
+  bool hasMoreMovies = true;
+
+  _MovieListState(MovieType movieType) {
+    _type = movieType;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getMovies(_currentPageSelected);
+  }
+
+  void getMovies(int pageNumber) async {
+    Future<page.Page> futurePage;
+    switch (_type) {
+      case MovieType.TOP_RATED_MOVIES:
+        futurePage = UseCaseFactory.getTopRatedUseCase().execute(pageNumber);
+        break;
+      case MovieType.POPULAR_MOVIES:
+        futurePage =
+            UseCaseFactory.getPopularMoviesUseCase().execute(pageNumber);
+        break;
+      default:
+        futurePage = null;
+        break;
+    }
+
+    if (futurePage != null) {
+      var page = await futurePage;
+      setState(() {
+        _movies.addAll(page.moviesList());
+        hasMoreMovies = page.hasMorePages();
+        _currentPageSelected++;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: _movies.length,
+      scrollDirection: Axis.vertical,
       itemBuilder: (context, position) => Container(
         padding: EdgeInsets.all(8),
         child: MovieListCell(_movies[position]),
--- flutter-version/ProgrammingChallenge/movie_db_app/lib/presentation/home_page_screen.dart
+++ flutter-version/ProgrammingChallenge/movie_db_app/lib/presentation/home_page_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:movie_db_app/core/use_case_factory.dart';
-import 'package:movie_db_app/domain/model/movie.dart';
 import 'package:movie_db_app/domain/model/movie_type.dart';
-import 'file:///C:/Users/gabri/Documents/svn/personal/MovieDBApi/flutter-version/ProgrammingChallenge/movie_db_app/lib/presentation/components/movieList/movie_list.dart';
+
+import 'components/movieList/movie_list.dart';
 
 class HomePageScreen extends StatefulWidget {
   @override
@@ -10,91 +9,63 @@ class HomePageScreen extends StatefulWidget {
 }
 
 class _HomePageScreenState extends State<HomePageScreen> {
-  List<Movie> _topRatedMovies = [];
-  List<Movie> _favoriteMovies = [];
-  List<Movie> _popularMovies = [];
+  int _currentTabIndex = 0;
+  PageController _pageController;
 
-  int _topRatedMoviesPageIndex = 1;
-  int _popularMoviesPageIndex = 1;
-
-  int _currentTabIndex;
-
-  _HomePageScreenState() {
-    _currentTabIndex = 0;
-    UseCaseFactory.getTopRatedUseCase().execute(_topRatedMoviesPageIndex).then(
-          (value) => this.updateTheState(
-            () {
-              _topRatedMovies.addAll(value.moviesList());
-            },
-          ),
-        );
-
-    UseCaseFactory.getPopularMoviesUseCase()
-        .execute(_popularMoviesPageIndex)
-        .then(
-          (value) => this.updateTheState(
-            () {
-              _popularMovies.addAll(value.moviesList());
-            },
-          ),
-        );
-  }
-
-  void updateTheState(VoidCallback action) {
-    setState(() {
-      action();
-    });
-  }
-
-  void _onTapItem(int index) {
-    updateTheState(() {
-      _currentTabIndex = index;
-    });
+  @override
+  void initState() {
+    _pageController = PageController(
+      initialPage: _currentTabIndex,
+    );
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _getCurrentFragment(),
-      bottomNavigationBar: BottomNavigationBar(
-        items: _getMenuItems(),
-        onTap: _onTapItem,
-        currentIndex: _currentTabIndex,
-      ),
+      body: buildPageView(),
+      bottomNavigationBar: buildBottomNavigationBar(),
     );
   }
 
-  Widget _getCurrentFragment() {
-    switch (_currentTabIndex) {
-      case TOP_RATED_MOVIES_TAB_INDEX:
-        return MovieList(_topRatedMovies);
-      case POPULAR_MOVIES_TAB_INDEX:
-        return MovieList(_popularMovies);
-      case FAVORITE_MOVIES_TAB_INDEX:
-        return MovieList(_favoriteMovies);
-      default:
-        return Text('aosdk');
-    }
+  BottomNavigationBar buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(MovieType.TOP_RATED_MOVIES.icon()),
+          label: MovieType.TOP_RATED_MOVIES.name(),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(MovieType.POPULAR_MOVIES.icon()),
+          label: MovieType.POPULAR_MOVIES.name(),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(MovieType.FAVORITE_MOVIES.icon()),
+          label: MovieType.FAVORITE_MOVIES.name(),
+        )
+      ],
+      onTap: (index) {
+        this._pageController.animateToPage(index,
+            duration: const Duration(milliseconds: 500),
+            curve: Curves.easeInOut);
+      },
+      currentIndex: _currentTabIndex,
+    );
   }
 
-  List<BottomNavigationBarItem> _getMenuItems() {
-    final movieTypes = [
-      MovieType.TOP_RATED_MOVIES,
-      MovieType.POPULAR_MOVIES,
-      MovieType.FAVORITE_MOVIES,
-    ];
-
-    return movieTypes
-        .map(
-          (e) => BottomNavigationBarItem(
-            icon: Icon(e.icon()),
-            label: e.name(),
-          ),
-        )
-        .toList();
+  PageView buildPageView() {
+    return PageView(
+      controller: _pageController,
+      onPageChanged: (newPage) {
+        setState(() {
+          this._currentTabIndex = newPage;
+        });
+      },
+      children: [
+        MovieList(MovieType.TOP_RATED_MOVIES),
+        MovieList(MovieType.POPULAR_MOVIES),
+        Text('Building')
+      ],
+    );
   }
 }
-
-const TOP_RATED_MOVIES_TAB_INDEX = 0;
-const POPULAR_MOVIES_TAB_INDEX = 1;
-const FAVORITE_MOVIES_TAB_INDEX = 2;
