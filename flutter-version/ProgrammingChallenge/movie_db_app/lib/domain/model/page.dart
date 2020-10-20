f538650629364af9cdf8fc7b99a21c6b5932fa29
Gabriel Bronzatti Moro <gabrielbronzattimoro@gmail.com>
2020-10-19 22:35:37 -0300

    Declaring the model using flutter syntax sugar

--- flutter-version/ProgrammingChallenge/movie_db_app/lib/domain/model/page.dart
+++ flutter-version/ProgrammingChallenge/movie_db_app/lib/domain/model/page.dart
@@ -3,22 +3,11 @@ import 'package:flutter/material.dart';
 import 'movie.dart';
 
 class Page {
-  List<Movie> _movieList;
-  bool _hasMorePages;
+  List<Movie> movieList;
+  bool hasMorePages;
 
   Page({
-    @required List<Movie> movies,
-    @required bool hasMorePages,
-  }) {
-    _movieList = movies;
-    _hasMorePages = hasMorePages;
-  }
-
-  moviesList() {
-    return _movieList;
-  }
-
-  hasMorePages() {
-    return _hasMorePages;
-  }
+    @required this.movieList,
+    @required this.hasMorePages,
+  });
 }
