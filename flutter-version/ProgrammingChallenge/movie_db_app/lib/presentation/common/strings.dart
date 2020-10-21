76d19771b0c6a0e39203cff87b713e17646016f6
Gabriel Bronzatti Moro <gabrielbronzattimoro@gmail.com>
2020-10-20 21:30:31 -0300

    Chaning the code to be more readable

--- flutter-version/ProgrammingChallenge/movie_db_app/lib/presentation/common/strings.dart
+++ flutter-version/ProgrammingChallenge/movie_db_app/lib/presentation/common/strings.dart
@@ -3,3 +3,5 @@ const String POPULAR = 'Popular';
 const String FAVORITE = 'Favorite';
 const String ORIGINAL_TITLE = 'Original Title';
 const String OVERVIEW_TITLE = 'Overview';
+const String LANGUAGE_TITLE = 'Language';
+const String POPULARITY_TITLE = 'Popularity';
--- flutter-version/ProgrammingChallenge/movie_db_app/lib/presentation/movie_details_screen.dart
+++ flutter-version/ProgrammingChallenge/movie_db_app/lib/presentation/movie_details_screen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:movie_db_app/domain/model/movie.dart';
 import 'package:movie_db_app/presentation/common/strings.dart';
 import 'package:movie_db_app/presentation/components/text_section_title_widget.dart';
-
 import 'components/text_section_label_widget.dart';
 
 // ignore: must_be_immutable
@@ -32,8 +31,12 @@ class _MovieDetailsScreen extends State<MovieDetailsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(_movie.title),
+      ),
       body: SingleChildScrollView(
         child: Container(
+          margin: const EdgeInsets.only(bottom: 40),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -42,44 +45,39 @@ class _MovieDetailsScreen extends State<MovieDetailsScreen> {
                   _movie.imageAddress(),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  top: 20,
-                  left: 20,
-                  right: 20,
-                ),
-                child: TextSectionLabel(
-                  ORIGINAL_TITLE,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 20,
-                  right: 20,
-                ),
-                child: TextSectionTitle(_movie.title),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  top: 20,
-                  left: 20,
-                  right: 20,
-                ),
-                child: TextSectionLabel(
-                  OVERVIEW_TITLE,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  left: 20,
-                  right: 20,
-                ),
-                child: Text(_movie.overview),
-              ),
+              _wrappingSectionLabel(TextSectionLabel(ORIGINAL_TITLE)),
+              _wrappingSectionContent(TextSectionTitle(_movie.title)),
+              _wrappingSectionLabel(TextSectionLabel(OVERVIEW_TITLE)),
+              _wrappingSectionContent(Text(_movie.overview)),
+              _wrappingSectionLabel(TextSectionLabel(LANGUAGE_TITLE)),
+              _wrappingSectionContent(Text(_movie.originalLanguage)),
+              _wrappingSectionLabel(TextSectionLabel(POPULARITY_TITLE)),
+              _wrappingSectionContent(Text(_movie.popularity.toString()))
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget _wrappingSectionLabel(Widget widget) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 20,
+        left: 20,
+        right: 20,
+      ),
+      child: widget,
+    );
+  }
+
+  Widget _wrappingSectionContent(Widget widget) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 20,
+        right: 20,
+      ),
+      child: widget,
     );
   }
 }
